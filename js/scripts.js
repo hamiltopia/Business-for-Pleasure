4ccb755367adad9062583aa0a6ee53cef14bafd6
github username <github email address>
2017-03-07 15:01:27 -0500

    Update top Header
    Site Brand and Nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
 
-	//ACCORDION
-	$('.ac-handle').click(function() {
-		$('.ac-handle').removeClass('open');
-		$('.ac-drawer').slideUp('normal');
 
-		if($(this).next().is(':hidden') == true) {
-			$(this).addClass('open');
-			$(this).next().slideDown('normal');
+  $(".navigation").hide();
+
+  $(".mobile-button").click(function() {
+		$(this).toggleClass("active");
+		$(".navigation").slideToggle("fast");
+	});
+
+
+	//ACCORDION
+	$(".ac-handle").click(function() {
+		$(".ac-handle").removeClass("open");
+		$(".ac-drawer").slideUp("normal");
+
+		if($(this).next().is(":hidden") == true) {
+			$(this).addClass("open");
+			$(this).next().slideDown("normal");
 		 }
 
 	 });
 
-   $('.ac-handle').mouseover(function() {
-  		$(this).addClass('hover');
+   $(".ac-handle").mouseover(function() {
+  		$(this).addClass("hover");
     }).mouseout(function() {
-    	$(this).removeClass('hover');
+    	$(this).removeClass("hover");
     });
 
-	$('.ac-drawer').hide();
+	$(".ac-drawer").hide();
   //ACCORDION
 
 
